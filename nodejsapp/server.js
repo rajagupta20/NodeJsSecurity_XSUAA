cdfedd95285c3966c5360c0800d20d7735200843
raja.gupta20 <raja.gupta20@gmail.com>
2021-08-11 08:59:53 +0000

    final

--- nodejsapp/server.js
+++ nodejsapp/server.js
@@ -1,11 +1,59 @@
+// const express = require('express');
+// const app = express();
+
+// app.get('/', function (req, res) {
+//   res.send('Hello World!');
+// });
+
+// const port = process.env.PORT || 3000;
+// app.listen(port, function () {
+//   console.log('myapp listening on port ' + port);
+// });
+
+//==================================================================
+
 const express = require('express');
+const { getProducts, getProductsByName} = require('./lib/repository');
 const app = express();
+const port = process.env.port || 8080;
+// secure the direct call to the application
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
 
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('myapp listening on port ' + port);
-});
+const passport = require('passport');
+const { JWTStrategy } = require('@sap/xssec');
+const xsenv = require('@sap/xsenv');
+
+// XSUAA Middleware
+passport.use(new JWTStrategy(
+                    xsenv.getServices(
+                            { 
+                                uaa: { tag: 'xsuaa' } 
+                            }
+                        ).uaa
+                    ));
+
+app.use(passport.initialize());
+app.use(passport.authenticate('JWT', { session: false }));
+
+
+
+app.get('/products', checkReadScope, getProducts);
+
+// Scope check
+function checkReadScope(req, res, next) {
+	if (req.authInfo.checkLocalScope('read')) {
+		return next();
+	} else {
+    	console.log('Missing the expected scope');
+    	res.status(403).end('Forbidden');
+	}
+}
+
+
+
+// Serve static files
+app.use('/', express.static('static/'));
+
+app.listen(port, () => {
+	console.log('%s listening at %s', app.name, port);
+})
